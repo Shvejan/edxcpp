a84d6e8aed4dc8c8ccd6667b06200f17353a0f0f
Shvejan <shvejan2011@gmail.com>
2019-10-19 21:34:22 +0530

    pointer

---
+++ point.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char const *argv[]) {
+  int n=3;
+  int *p=&n;
+  std::cout << p << '\n';
+  std::cout << *p << '\n';
+  return 0;
+}
